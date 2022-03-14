7c747423861a0a90947fed89b37f6ac03bcdc1f2
BayramGUN <69740046+BayramGUN@users.noreply.github.com>
2022-03-15 02:05:02 +0300

    Add files via upload

---
+++ not_ortalamasi.java
@@ -0,0 +1,35 @@
+package com.example;
+import java.util.*;
+
+public class not_ortalamasi {
+    public static void main(String[] args) {
+        int average;
+        int math, music, chem, physic, turkish, hist;
+        Scanner num = new Scanner(System.in);
+
+        System.out.println("Matematik Notunuzu Giriniz: ");
+        math = num.nextInt();
+
+        System.out.println("Fizik Notunuzu Giriniz: ");
+        physic = num.nextInt();
+
+        System.out.println("Kimya Notunuzu Giriniz: ");
+        chem = num.nextInt();
+
+        System.out.println("Turkçe Notunuzu Giriniz: ");
+        turkish = num.nextInt();
+
+        System.out.println("Tarih Notunuzu Giriniz: ");
+        hist = num.nextInt();
+
+        System.out.println("Muzik Notunuzu Giriniz: ");
+        music = num.nextInt();
+        average = (math + music + hist + physic + chem + turkish) / 6;
+        System.out.println("Notlarin ortalamasi: " + average);
+        if(average >= 60) {
+            System.out.println("Sinifi Gecti");
+        } else
+            System.out.println("Sinifta Kaldi");
+
+    }
+}
