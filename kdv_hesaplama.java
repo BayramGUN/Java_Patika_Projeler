4c7e6299511db6a723af33a73365897798d08036
BayramGUN <69740046+BayramGUN@users.noreply.github.com>
2022-03-15 02:15:03 +0300

    Add files via upload

---
+++ kdv_hesaplama.java
@@ -0,0 +1,16 @@
+package com.example;
+import java.util.*;
+
+public class kdv_hesaplama {
+    public static void main(String[] args) {
+        double price, kdv_tax = 0.18, tax_price, new_price;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Tutar giriniz: ");
+        price = input.nextDouble();
+        tax_price = price * kdv_tax;
+        new_price = tax_price + price;
+        System.out.println("Urun fiyati: " + price);
+        System.out.println("KDV Tutari: " + tax_price);
+        System.out.println("Urunun KDV li fiyati: " + new_price);
+    }
+}
